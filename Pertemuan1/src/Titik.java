8e007a6fe0eaf14b806ae0a1d1dfe95ad8da0611
Maulana Ghazzam Adil Al Faiq <maulanaghazzamaaf@gmail.com>
2026-02-24 13:23:54 +0700

    ulang

--- Pertemuan1/src/Titik.java
+++ Pertemuan1/src/Titik.java
@@ -3,53 +3,53 @@
  * Pembuat       : Maulana Ghazzam Adil Al Faiq - 24060124130083
  * Tanggal       : 20 Februari 2026
  */
+/* Nama File     : Titik.java
+ * Deskripsi     : berisi atribut dan method dalam class Titik
+ * Pembuat       :
+ * Tanggal       :
+ */
 
 public class Titik {
-    /*ATRIBUT*/
+
+    /***************ATRIBUT*****************/
     double absis;
     double ordinat;
 
-    /*METHOD*/
-    // Konstruktor
+    /***************METHOD******************/
+    //konstruktor untuk membuat titik (0,0)
     Titik(){
         absis = 0;
         ordinat = 0;
     }
 
-    // Selektor
+    //mengembalikan nilai absis
     double getAbsis(){
         return absis;
     }
 
+    //mengembalikan nilai ordinat
     double getOrdinat(){
         return ordinat;
     }
 
-    // Mutator
+    //mengeset absis titik dengan nilai baru x
     void setAbsis(double x){
         absis = x;
     }
+
+    //mengeset ordinat titik dengan nilai baru y
     void setOrdinat(double y){
         ordinat = y;
     }
 
-    // Operator
+    //menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
     void geser(double x, double y){
         absis = absis + x;
         ordinat = ordinat + x;
     }
 
+    //mencetak koordinat titik
     void printTitik(){
         System.out.println("Titik (" + absis + "," + ordinat + ")");
-    }
-
-    public static void main(String[] args) {
-        Titik T1 = new Titik();
-        T1.setAbsis(3);
-        T1.setOrdinat(4);
-        T1.printTitik();
-
-        T1.geser(3,4);
-        T1.printTitik();
     }
 }
